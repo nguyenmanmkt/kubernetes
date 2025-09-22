5a9236391cbe53de37179176b98d87a5c3d34d81
nguyenmanmkt <129373032+nguyenmanmkt@users.noreply.github.com>
2025-09-22 09:01:34 +0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 git init
 git add .
-git config --global user.email "nguyenmanmkt@gmail.com"
-git config --global user.name "nguyenmanmkt"
+git config --global user.email ""
+git config --global user.name ""
 git remote add origin https://github.com/nguyenmanmkt/kubernetes.git
 git push -u origin main
